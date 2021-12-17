3561498773a51361b7a0d824e994dd69615b5282
elijafari <el.jafari.el@gmail.com>
2021-12-16 16:00:12 -0800

    based on wirefarme production page is done although there is a problem in slideshow that I should figure it out

--- script.js
+++ script.js
@@ -17,6 +17,22 @@ function showDivs(n) {
 function save(){
 document.addEventListener("click", e => {
     e.preventDefault()
-    document.getElementById('save').src=`star-purple.png`
+    document.getElementById('save').src=`starPurple.png`
 })
 }
+
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 49.263950, lng: -122.876442 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("googleMap"), {
+      zoom: 15,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
+  
